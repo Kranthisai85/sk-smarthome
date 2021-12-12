920980e8c1ad48cf475a07f04a40080c7d62725e
varshitratna <82994300+varshitratna@users.noreply.github.com>
2021-12-13 01:53:25 +0530

    initial commit

---
+++ logs/1.joomla_update.php
@@ -0,0 +1,24 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2020-07-14 02:48:51 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2020-07-14T02:48:51+00:00	INFO ::1	update	Update started by user Super User (568). Old version is 3.9.18.
+2020-07-14T02:48:51+00:00	INFO ::1	update	Downloading update file from https://downloads.joomla.org/cms/joomla3/3-9-19/Joomla_3.9.19-Stable-Update_Package.zip.
+2020-07-14T02:49:31+00:00	INFO ::1	update	File Joomla_3.9.19-Stable-Update_Package.zip downloaded.
+2020-07-14T02:49:32+00:00	INFO ::1	update	Starting installation of new version.
+2020-07-14T02:50:36+00:00	INFO ::1	update	Finalising installation.
+2020-07-14T02:50:36+00:00	INFO ::1	update	Ran query from file 3.9.19-2020-05-16. Query text: ALTER TABLE `#__ucm_content` MODIFY `core_title` varchar(400) NOT NULL DEFAULT '.
+2020-07-14T02:50:36+00:00	INFO ::1	update	Ran query from file 3.9.19-2020-06-01. Query text: INSERT INTO `#__postinstall_messages` (`extension_id`, `title_key`, `description.
+2020-07-14T02:50:36+00:00	INFO ::1	update	Deleting removed files and folders.
+2020-07-14T02:50:52+00:00	INFO ::1	update	Cleaning up after installation.
+2020-07-14T02:50:52+00:00	INFO ::1	update	Update to version 3.9.19 is complete.
+2020-08-06T07:03:07+00:00	INFO ::1	update	Update started by user Super User (568). Old version is 3.9.19.
+2020-08-06T07:03:07+00:00	INFO ::1	update	Downloading update file from https://downloads.joomla.org/cms/joomla3/3-9-20/Joomla_3.9.20-Stable-Update_Package.zip.
+2020-08-06T07:03:34+00:00	INFO ::1	update	File Joomla_3.9.20-Stable-Update_Package.zip downloaded.
+2020-08-06T07:03:34+00:00	INFO ::1	update	Starting installation of new version.
+2020-08-06T07:04:37+00:00	INFO ::1	update	Finalising installation.
+2020-08-06T07:04:37+00:00	INFO ::1	update	Deleting removed files and folders.
+2020-08-06T07:04:39+00:00	INFO ::1	update	Cleaning up after installation.
+2020-08-06T07:04:39+00:00	INFO ::1	update	Update to version 3.9.20 is complete.
